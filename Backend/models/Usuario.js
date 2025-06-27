aa9d114e0ab5368beb5ed273927914ebff73907f
Victor Escalera Solis <victor.solis2019@gmail.com>
2025-06-26 18:29:22 -0600

    terecer

---
+++ Backend/models/Usuario.js
@@ -0,0 +1,10 @@
+// Backend/models/Usuario.js
+const mongoose = require('mongoose');
+
+const usuarioSchema = new mongoose.Schema({
+  nombre: String,
+  email: String,
+  password: String
+});
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
